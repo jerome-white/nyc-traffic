975797c79c29c61cb0d1cab7b62afd53692938e6
Jerome White <jerome@thewex.net>
2015-07-22 17:09:44 +0400

    Prediction plotting does a generic split (not just by the neighbor specification (and assuming a two-neighbor specification)). The split can be specified from the command line.

--- src/prediction/plot.py
+++ src/prediction/plot.py
@@ -13,17 +13,18 @@ def plotter(fname, data, args):
 ext_ = '.pdf'
 user = cli.CommandLine(cli.optsfile('prediction-plot'))
 
-df = pd.DataFrame.from_csv(user.args.data, sep=';', index_col=None)
-grouped = df.groupby(['neighbors', 'node'])['matthews_corrcoef']
+df = pd.DataFrame.from_csv(user.args.data, sep=';', index_col=None).dropna()
+grouped = df.groupby(user.args.gfilter + ['node'])['matthews_corrcoef']
 vals = grouped.agg([np.mean, np.std])
 
-splt = []
-for i in range(vals.ix.ndim):
-    segment = vals.loc[[i]].reset_index(level='neighbors', drop=True)
-    splt.append(segment)
+for i in vals.index.levels[:-1]:
+    args = { 'level': user.args.gfilter, 'drop': True }
+    splt = [ vals.loc[x].reset_index(**args) for x in i ]
 (zero, one) = splt
 
-# plot the zero/1-level neighbor comparison
+#
+# plot a comparison between the filters
+#
 z = pd.merge(zero, one, left_index=True, right_index=True)
 z = z.sort('mean_x', ascending=False)
 fname = path.join(user.args.output, 'compare' + ext_)
@@ -36,21 +37,25 @@ args = {
     'ylim': (-1, 1),
     }
 plotter(fname, view, args)
- 
-# plot the zero-neighbors in ascending order
-s = zero.sort('mean')#.groupby(['cluster'])
-fname = path.join(user.args.output, 'zero' + ext_)
-view = s['mean']
-args = {
-    'figsize': (40, 100),
-    'fontsize': 36,
-    'kind': 'barh',
-    'xerr': s['std'],
-    'xlim': (-1, 1),
-    }
-plotter(fname, view, args)
 
+#
+# plot the zero-neighbors in ascending order
+#
+# zsrt = zero.sort('mean')#.groupby(['cluster'])
+# fname = path.join(user.args.output, 'zero' + ext_)
+# view = zsrt['mean']
+# args = {
+#     'figsize': (40, 100),
+#     'fontsize': 36,
+#     'kind': 'barh',
+#     'xerr': zsrt['std'],
+#     'xlim': (-1, 1),
+#     }
+# plotter(fname, view, args)
+
+#
 # plot the performance difference
+#
 fname = path.join(user.args.output, 'diff' + ext_)
 view = one['mean'] - zero['mean']
 view.sort('mean', ascending=False)
@@ -66,17 +71,26 @@ for i in user.args.clusters:
     cf = pd.DataFrame.from_csv(i)
     view = pd.merge(vals, cf, left_index=True, right_index=True)
 
-    view = view.reset_index(level='node', drop=True)
-    view.loc[[1],'cluster'] += 0.5
+    view.reset_index(level='node', drop=True, inplace=True)
+    idxs = view.index.unique()
+    for (j, k) in enumerate(idxs):
+        view.loc[k,'cluster'] += j / len(idxs)
 
     f = path.basename(i) + ext_
     fname = path.join(user.args.output, f)
 
     ax = None
-    for j in range(view.ix.ndim):
-        v = view.loc[[j]]
-        ax = v.plot(kind='scatter', x='cluster', y='mean',
-                    label=str(j), color=colors[j], ax=ax)
+    args = {
+        'kind': 'scatter',
+        'x': 'cluster',
+        'y': 'mean',
+        'xlim': (view.cluster.min(), view.cluster.max()),
+        'ax': ax,
+        }
+    assert(len(idxs) == len(colors))
+    for (j, k) in zip(idxs, colors):
+        v = view.loc[j]
+        ax = v.plot(label=str(j), color=k, **args)
 
         print(f, j, v['mean'].corr(v['cluster'], method='pearson'))
     assert(ax)
--- src/prediction/plot.sh
+++ src/prediction/plot.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-logs=(
-    2015-0715-113928,6
-)
+toopts() {
+    opt=--$1
+    shift
+    echo "$opt `sed -e"s/ / $opt /g" <<< $@`"
+    return
+}
+
+while getopts "l:f:" OPTION; do
+    case $OPTION in
+	l) logs=( ${logs[@]} $OPTARG ) ;;
+	f) filters=( ${filters[@]} $OPTARG ) ;;
+	*) exit 1 ;;
+    esac
+done
 traffic=$NYCTRAFFIC/src
 prediction=$traffic/prediction
 cluster=$traffic/cluster
 
 for i in ${logs[@]}; do
+    echo $i
+    
     l=( `sed -e's/,/ /g' <<< $i` )
     dat=$prediction/log/${l[0]}/dat
 
-    cls=( `ls $cluster/log/${l[1]}/dat-*` )
+    cls=( `ls $cluster/log/${l[1]}/dat-* 2> /dev/null` )
+    if [ ! $cls ]; then
+	continue
+    fi
     output=`dirname $dat`/fig/$k
     mkdir --parents $output
 
     python3 $prediction/plot.py \
 	    --data $dat \
 	    --output $output \
-    	    --clusters `sed -e's/ / --clusters /g' <<< ${cls[@]}`
+	    `toopts clusters ${cls[@]}` \
+	    `toopts filter ${filters[@]}` || \
+	break
 done
