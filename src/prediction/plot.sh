29e7051a27ba94a62ed4e20c1db99666c97c7cb0
Jerome White <jerome@thewex.net>
2015-07-23 17:34:30 +0400

    Improvements to prediction plotting output and data selection.

--- src/prediction/plot.sh
+++ src/prediction/plot.sh
@@ -7,34 +7,48 @@ toopts() {
     return
 }
 
+traffic=$NYCTRAFFIC/src
 while getopts "l:f:" OPTION; do
     case $OPTION in
 	l) logs=( ${logs[@]} $OPTARG ) ;;
 	f) filters=( ${filters[@]} $OPTARG ) ;;
+	h)
+	    cat <<EOF
+$0 [options]
+ -l log directory and neighbors: "dir,n"
+ -f filters
+
+Both can be specified multiple times; all filters will be applied to
+all log directories.
+EOF
+	    exit
+	    ;;
 	*) exit 1 ;;
     esac
 done
-traffic=$NYCTRAFFIC/src
-prediction=$traffic/prediction
-cluster=$traffic/cluster
 
+prediction=$traffic/prediction
 for i in ${logs[@]}; do
     echo $i
     
     l=( `sed -e's/,/ /g' <<< $i` )
-    dat=$prediction/log/${l[0]}/dat
+    dat=$prediction/${l[0]}/dat
 
-    cls=( `ls $cluster/log/${l[1]}/dat-* 2> /dev/null` )
+    cls=( `ls $traffic/cluster/log/${l[1]}/dat-* 2> /dev/null` )
     if [ ! $cls ]; then
 	continue
     fi
-    output=`dirname $dat`/fig/$k
+    fig=${l[1]}.`sed -e's/ /\./g' <<< ${filters[@]}`
+    output=`dirname $dat`/fig/$fig
     mkdir --parents $output
 
     python3 $prediction/plot.py \
 	    --data $dat \
-	    --output $output \
+	    --stats-file $output/stats.csv \
+	    --plot-directory $output \
 	    `toopts clusters ${cls[@]}` \
-	    `toopts filter ${filters[@]}` || \
+	    `toopts filter ${filters[@]}` || { \
+	rm --recursive --force $output
 	break
+    }
 done
