a347912bceddbdde7d3ea6b2689cfb22b46914a5
Jerome White <jerome@thewex.net>
2015-08-05 17:43:08 +0400

    Prediction plotting handles ANOVA. Uses Pandas group-by rather than indexing. Does not handle non-window related filters.

--- src/prediction/plot.py
+++ src/prediction/plot.py
@@ -1,104 +1,83 @@
+import os
+
 import numpy as np
 import pandas as pd
-import os.path as path
-import matplotlib.pyplot as pyp
 
 from scipy import stats
+from pathlib import Path
 
 from lib import cli
 from lib import utils
-from lib.csvwriter import CSVWriter
+# from lib.csvwriter import CSVWriter
 
 def plotter(fname, data, args):
     plt = data.plot(**args)
     utils.mkplot_(plt, fname)
 
-ext_ = '.pdf'
+def comparison(df, args, output_d, ext='pdf'):
+    '''
+    plot a comparison between the filters
+    '''
+    fname = path.join(output_d, '.'.join([ 'compare', ext ]))
+    df = df.sort([('mean', df['mean'].columns[0])], ascending=False)
+    plotter(fname, df['mean'], args)
+
+def performance(df, args, output_d, ext='pdf'):
+    '''
+    plot the performance difference
+    '''
+    args.update({ 'ylim': (-1, 1) }) # XXX this should be a copy
+    df_mean = df['mean']
+    base = df_mean.columns[0]
+    for i in df_mean.columns[1:].tolist():
+        f = 'diff.{0}-{1}{2}'.format(i, base, ext)
+        fname = path.join(output_d, f)
+        view = df_mean[i] - df_mean[base]
+        view.sort(ascending=False)
+        plotter(fname, view.dropna(), args)
+
+def anova(df, cluster_directory):
+    def issig(val, sig=0.05):
+        return '*' if val < sig else ' '
+    
+    for i in df.columns:
+        args = [ str(x) for x in (10, i, 5, -0.002) ]
+
+        path = Path(cluster_directory, *args)
+        for pth in path.iterdir():
+            if pth.suffix != '.csv':
+                continue
+            data = pd.DataFrame.from_csv(str(pth))
+            cf = pd.concat((df[i], data), axis=1).dropna()
+            groups = cf.groupby(['cluster'])[i]
+
+            samples = [ x.values for (_, x) in groups ]
+            oneway = stats.f_oneway(*samples)
+            kruskal = stats.kruskal(*samples)
+
+            msg = '{0:2d} {1:2d} '.format(i, len(groups))
+            # msg = '{0} {1:2d} '.format(i, len(groups))
+            for (v, p) in (oneway, kruskal):
+                msg += '{0:6.3f} {1:.3f} {2} '.format(v, p, issig(p))
+            print(msg)
+    
 user = cli.CommandLine(cli.optsfile('prediction-plot'))
 
-df = pd.DataFrame.from_csv(user.args.data, sep=';', index_col=None)
-assert(all([ x in df.columns for x in user.args.gfilter]))
-    
-df = df.loc[df['confusion_matrix'] != np.nan]
-grouped = df.groupby(user.args.gfilter + ['node'])['matthews_corrcoef']
+raw = pd.DataFrame.from_csv(user.args.data, sep=';', index_col=None)
+assert(all([ x in raw.columns for x in user.args.gfilter]))
+raw = raw.loc[raw['confusion_matrix'] != np.nan]
+
+grouped = raw.groupby(user.args.gfilter + ['node'])['matthews_corrcoef']
 df = grouped.agg([np.mean, stats.sem]).unstack(0)
 
-#
-# plot a comparison between the filters
-#
-fname = path.join(user.args.plotdir, 'compare' + ext_)
-df = df.sort([('mean', df['mean'].columns[0])], ascending=False)
 args = {
     'figsize': (400, 40),
     'fontsize': 96,
     'kind': 'bar',
     'yerr': df['sem'],
     'ylim': (0, 1),
-    }
-plotter(fname, df['mean'], args)
+}
+# comparison(df, args, user.args.plotdir)
+# performance(df, args, user.args.plotdir)
 
-#
-# plot the zero-neighbors in ascending order
-#
-# zsrt = zero.sort('mean')#.groupby(['cluster'])
-# fname = path.join(user.args.plotdir, 'zero' + ext_)
-# view = zsrt['mean']
-# args = {
-#     'figsize': (40, 100),
-#     'fontsize': 36,
-#     'kind': 'barh',
-#     'xerr': zsrt['std'],
-#     'xlim': (-1, 1),
-#     }
-# plotter(fname, view, args)
-
-#
-# plot the performance difference
-#
-args.update({ 'ylim': (-1, 1) })
-df_mean = df['mean']
-base = df_mean.columns[0]
-for i in df_mean.columns[1:].tolist():
-    f = 'diff.{0}-{1}{2}'.format(i, base, ext_)
-    fname = path.join(user.args.plotdir, f)
-    view = df_mean[i] - df_mean[base]
-    view.sort(ascending=False)
-    plotter(fname, view.dropna(), args)
-
-exit()
-
-colors = ['DarkBlue', 'DarkGreen', 'DarkRed']
-csvheader = ['group', 'filter', 'correlation']
-args = { 'kind': 'scatter', 'x': 'cluster', 'y': 'mean' }
-
-with CSVWriter(csvheader, user.args.stfile) as csv:
-    csv.writeheader()
-    for i in user.args.gfilter:
-        for j in df[i].unique():
-            cf = pd.DataFrame.from_csv(j)
-            view = pd.merge(df_mean, cf, left_index=True, right_index=True)
-
-            view.reset_index(level='node', drop=True, inplace=True)
-            idxs = view.index.unique()
-            for (x, y) in enumerate(idxs):
-                view.loc[y, 'cluster'] += x / len(idxs)
-
-                f = path.basename(i) + ext_
-                fname = path.join(user.args.plotdir, f)
-
-                ax = None
-                args.update({
-                    'xlim': (view.cluster.min(), view.cluster.max()),
-                    'ax': ax,
-                })
-                assert(len(idxs) == len(colors))
-        
-                for (x, y) in zip(idxs, colors):
-                    v = view.loc[x]
-                    ax = v.plot(label=str(x), color=y, **args)
-                    corr = v['mean'].corr(v['cluster'], method='pearson')
-                    
-                    row = [ f, j, corr ]
-                    csv.writerow(dict(zip(csvheader, row)))
-                assert(ax)
-                utils.mkplot_(ax, fname)
+anova(df['mean'], user.args.clusters)
