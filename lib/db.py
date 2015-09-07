22edb55d192ac3548b96e4921874e2ab4d95735b
Jerome White <jerome@thewex.net>
2015-09-07 22:31:06 +0400

    Added socket option to database connection. Added 'mark' function to grab database info for logging.

--- lib/db.py
+++ lib/db.py
@@ -9,9 +9,23 @@ class Database:
         
 class DatabaseConnection(Database):
     def __init__(self, host='localhost', user='reader', db='traffic'):
-        kwargs = { 'host': host, 'user': user, 'db': db, 'autocommit': True }
+        kwargs = {
+            'host': host,
+            'user': user,
+            'db': db,
+            'autocommit': True,
+            'unix_socket': '/usr/local/mysql/data/mysql.sock',
+        }
         self.resource = pymysql.connect(**kwargs)
 
 class DatabaseCursor(Database):
     def __init__(self, connection):
         self.resource = connection.cursor(pymysql.cursors.DictCursor)
+
+def mark():
+    with DatabaseConnection() as connection:
+        with DatabaseCursor(connection) as cursor:
+            cursor.execute('SELECT MAX(as_of) AS mark FROM reading')
+            row = cursor.fetchone()
+            
+            return row['mark']
