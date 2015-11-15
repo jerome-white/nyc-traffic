280c533771705f8f7d3b4dadb5292932084bf012
Jerome White <jerome@thewex.net>
2015-11-15 09:02:53 +0400

    Made more robust (in the style of prediction's main): puts results in directory based on date.

--- src/characterise/characterise.sh
+++ src/characterise/characterise.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-dir=$NYCTRAFFICLOG/characterise
-mkdir --parents $dir
+mkcurrent() {
+    cd $1
+    rm --force current
+    ln --symbolic `basename $out` current
+}
+
+mklogdir() {
+    out=/
+    while [ -e $out ]; do
+	out=$1/`date +%Y_%m-%d_%H%M`.`hostname`
+	sleep 1
+    done
+    mkdir --parents $out
+    ( mkcurrent `dirname $out` )
+    
+    echo $out
+}
+
+dir=`mklogdir $NYCTRAFFICLOG/characterise`
+# dir=$NYCTRAFFICLOG/characterise/2015_11-06_0340.samjam.local
 pkl=$dir/observations.pkl
 
 python3 $NYCTRAFFIC/src/characterise/characterise.py \
-    	--prediction-window 10 \
-    	--target-window 10 \
-    	--speed-threshold -0.5 \
+    	--prediction-window 11 \
+    	--target-window 11 \
+    	--speed-threshold -0.002 \
 	--reporting-threshold 120 \
 	--figure-directory $dir \
-	--pickle $pkl 2> $dir/log
+ 	--pickle $pkl &> $dir/log
+#	--resume $pkl
+
