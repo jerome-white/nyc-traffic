e8de2204a4b332d4f82e80723f172b0273134c09
Jerome White <jerome@thewex.net>
2015-11-15 08:47:16 +0400

    Iterating a window means going through each value.

---
+++ lib/window.py
@@ -0,0 +1,30 @@
+class Window:
+    def __init__(self, observation, prediction, target):
+        self.observation = observation
+        self.prediction = prediction
+        self.target = target
+
+        self.__elements = []
+
+    def __len__(self):
+        return self.observation + self.prediction + self.target
+
+    def __repr__(self):
+        m = map(str, [ self.observation, self.prediction, self.target ])
+        return ','.join(m)
+
+    def __str__(self):
+        return repr(self)
+
+    def __iter__(self):
+        self.__elements = [ self.observation, self.prediction, self.target ]
+        return self
+
+    def __next__(self):
+        if self.__elements:
+            return self.__elements.pop(0)
+        
+        raise StopIteration
+    
+    def tail(self):
+        return Window(self.target, self.prediction, self.target)
