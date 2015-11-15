1da4f24be6ffccc73867aa592982901b2eb8802e
Jerome White <jerome@thewex.net>
2015-11-15 07:54:29 +0400

    Change point identification moved to object interface for easier swapping by users.

--- lib/cpoint.py
+++ lib/cpoint.py
@@ -1,28 +1,69 @@
+import warnings
+import operator
+
+import numpy as np
 import scipy.constants as constant
 
-def to_gravity(duration, before, after):
-    '''
-    Assume x and y to be miles per hour, and t to be minutes
-    '''
-    meters = (after - before) * constant.mile # miles to meters
-    seconds = duration * constant.minute # minutes to seconds
-    acceleration = meters / constant.hour / seconds
+class ChangePoint:
+    def __init__(self, threshold, categories=2):
+        self.threshold = threshold
+        self.categories = categories
+
+    def change(self, duration, before, after):
+        raise NotImplementedError()
     
-    return acceleration / constant.g
+    def classify(self, duration, before, after):
+        '''
+        duration: minutes
+        before, after: miles per hour
+        '''
+        if self.threshold < 0:
+            args = [ before, after ]
+            f = operator.le
+        else:
+            args = [ after, before ]
+            f = operator.gt
+        diff = self.change(duration, *args)
 
-def compare(measurement, threshold, direction):
-    allowable = measurement <= threshold
+        return f(diff, self.threshold)
+
+class Acceleration(ChangePoint):
+    def change(self, duration, before, after):
+        meters = (after - before) * constant.mile # miles to meters
+        seconds = duration * constant.minute # minutes to seconds
+        acceleration = meters / constant.hour / seconds
     
-    return allowable if direction else not allowable
+        return acceleration / constant.g
 
-def changed(duration, before, after, threshold):
-    if threshold < 0:
-        allowable = to_gravity(duration, before, after) <= threshold
-    else:
-        allowable = to_gravity(duration, after, before) > threshold
+    # def compare(measurement, threshold, direction):
+    #     allowable = measurement <= threshold
+        
+    #     return allowable if direction else not allowable
 
-    return allowable
 
+class Percentage(ChangePoint):
+    def change(self, duration, before, after):
+        return (after - before) / before
+
+class Derivative(ChangePoint):
+    def __init__(self, threshold, categories=2, order=1):
+        super().__init__(threshold, categories)
+        self.order = order
+
+    def change(self, duration, before, after):
+        rate = constant.mile / constant.hour
+        x = (0, before * rate)
+        y = (duration, after * rate)
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings('error')
+            try:
+                (slope, *_) = np.polyfit(x, y, self.order)
+            except (RuntimeWarning, numpy.RankWarning) as error:
+                raise ValueError()
+
+        return slope
+    
 def bucket(tstamp):
     assert(tstamp.second == 0)
     return tstamp.hour * 60 + tstamp.minute
