1d14cdb045dc6c2bfc8406786c38bc44cb5f1a42
Jerome White <jerome@thewex.net>
2016-04-14 08:18:56 +0400

    Generating the operational table comes after log file creation but before running main. This is more appropriate for distributed runs.
    Also, configuration files now placed in log directory.

--- src/prediction/main.sh
+++ src/prediction/main.sh
@@ -14,7 +14,7 @@ phome=$NYCTRAFFIC/src/prediction
 while true; do
     out=$NYCTRAFFICLOG/prediction/`date +%Y_%m-%d_%H%M`.`hostname`
     if [ ! -e $out ]; then
-        mkdir --parents $out
+        mkdir --parents $out/ini
         ( mkcurrent `dirname $out` )
         break
     fi
@@ -25,18 +25,20 @@ done
 #
 # Begin the run!
 #
-tmp=`mktemp --directory`
 python3 $phome/mkconfigs.py \
     --reporting-threshold 120 \
-    --output-directory $out \
+    --output-directory $out/ini \
     --skeleton $NYCTRAFFIC/etc/opts/prediction.ini \
     --parallel \
     --quiet
 
 i=0
-configs=( `ls $tmp/*` )
+configs=( `ls $out/ini/*` )
 for j in ${configs[@]}; do
     echo "[ `date` : $i / ${#configs[@]} ] $j " >> $out/trace
+    
+    grep --quiet 'node' $j || python3 $NYCTRAFFIC/etc/db/mkopt.py --config $j
     python3 $phome/main.py --config $j
+    
     (( i++ ))
 done > $out/dat 2> $out/log
