c0b2dff0a2d61cf8792ac1040cc331cc072e30b3
Jerome White <jerome@thewex.net>
2016-06-14 15:21:34 +0400

    Builds box plot for all window pairs in the database rather than relying on a specific window pair from the command line. Alleviatest the burden on the user of either knowing the window pair they want, or using some other means to get all valid pairs. Assumes building all the plots isn't that computationally expensive.

--- src/characterise/occurence-plot.py
+++ src/characterise/occurence-plot.py
@@ -3,27 +3,43 @@ import pandas as pd
 import seaborn as sns
 
 from lib import db
-form lib import cli
+from lib import cli
+from pathlib import Path
 
 args = cli.CommandLine('prediction-plot').args
+root = Path(args.plotdir)
 
-sql = [ 'SELECT as_of, node, jam',
-        'FROM occurrence',
-        'WHERE observation = {0} AND prediction = {1}',
-        'GROUP BY node',
-        ]
-sql = db.process(sql, args.window_obs, args.window_pred)
+occurrences = [ 'SELECT as_of, node, jam',
+                'FROM occurrence',
+                'WHERE observation = {0} AND prediction = {1}',
+                'GROUP BY node',
+                ]
 
 with db.DatabaseConnection() as con:
-    df = pd.read_sql_query(sql, con=con, index_col=[ 'as_of', 'node' ])
-    df = df.unstack(level='node')
-    df = df.resample(args.gfilter).mean()
-grouped = df.groupby(lambda x: x.hour).mean()
-grouped = grouped.stack(level='node')
+    with db.DatabaseCursor(con) as cursor:
+        args = { 'x': 'as_of', 'y': 'jam' }
+        # XXX should select all unique pairs
+        sql = [ 'SELECT observation, prediction',
+                'FROM occurrence',
+                ]
+        sql = db.process(sql)
+        cursor.execute(sql)
+        
+        for row in cursor:
+            windows = [ row[x] for x in [ 'observation', 'prediction' ]]
+            sql = db.process(occurrences, *windows)
 
-args = { 'x': 'as_of', 'y': 'jam', 'data': grouped }
-sns.boxplot(palette="PRGn", whis=np.inf, **args)
-sns.stripplot(jitter=True, size=3, color='.3', linewidth=0, **args)
-sns.despine(offset=10, trim=True)
+            df = pd.read_sql_query(sql, con=con, index_col=[ 'as_of', 'node' ])
+            df = df.unstack(level='node')
+            df = df.resample(args.gfilter).sum()
+            grouped = df.groupby(lambda x: x.hour).mean()
+            grouped = grouped.stack(level='node')
 
-plt.gcf().savefig(args.plotname)
+            args['data'] = grouped
+            sns.boxplot(palette="PRGn", whis=np.inf, **args)
+            sns.stripplot(jitter=True, size=3, color='.3', linewidth=0, **args)
+            sns.despine(offset=10, trim=True)
+
+            fname = '-'.join(windows)
+            relative = Path(root, fname).with_suffix('.png')
+            plt.gcf().savefig(str(relative))
