d5f3b9e25a9ef484f10cc4065a80729a0309c494
Jerome White <jerome@thewex.net>
2016-06-16 14:39:40 +0400

    Returned nodes being ordered is optional.

--- lib/ngen.py
+++ lib/ngen.py
@@ -8,13 +8,12 @@ class NodeGenerator:
                 yield tup
                 
     def getnodes(self, connection=None):
-        if not connection:
+        if connection:
+            with db.DatabaseCursor(connection) as cursor:
+                yield from self._getnodes(cursor)
+        else:
             with db.DatabaseConnection() as conn:
                 yield from self.getnodes(conn)
-            return
-        
-        with db.DatabaseCursor(connection) as cursor:
-            yield from self._getnodes(cursor)
 
     def _getnode(self, cursor):
         raise NotImplementedError()
@@ -40,12 +39,13 @@ class SequentialGenerator(NodeGenerator):
     def __init__(self, table='operational'):
         self.table = table
         
-    def _getnodes(self, cursor):
+    def _getnodes(self, cursor, order=False):
         sql = [ 'SELECT id',
                 'FROM {0}',
-                'ORDER BY id ASC',
                 ]
-        
+        if order:
+            sql.append('ORDER BY id ASC')
+
         cursor.execute(db.process(sql, self.table))
         for row in cursor:
             yield row['id']
