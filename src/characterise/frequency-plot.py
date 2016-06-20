e604835bd23fc9e881ef2252394ee47f03eeb599
Jerome White <jerome@thewex.net>
2016-06-20 17:07:38 +0400

    Better logging.

--- src/characterise/frequency-plot.py
+++ src/characterise/frequency-plot.py
@@ -17,33 +17,35 @@ def mkargs(top_level, freq):
                 if j.is_dir():
                     yield (j, freq)
 
-def accumulate(path, freq):
+def accumulate(args):
+    (path, freq) = args
     (observation, prediction) = [ int(x) for x in path.parts[-2:] ]
 
+    logger.getlogger().info('o: {0} p: {1}'.format(observation, prediction))
+    
     data = [ pd.read_pickle(str(x)) for x in path.glob('*.pkl') ]
     df = pd.concat(data, axis=1)
     df = df.resample(freq).sum().mean()
 
     return (observation, prediction, df.mean())
 
-log = logger.getlogger(True)
-
 args = cli.CommandLine(cli.optsfile('characterisation-plot')).args
 top_level = Path(args.source)
 target = Path(args.target)
 target.mkdir(parents=True, exist_ok=True)
 
-frequencies = args.freqs if args.freqs else [ 'D' ] # XXX defaults?
+freqs = args.freqs if args.freqs else [ 'D' ] # XXX defaults?
 
-for freq in args.freqs:
-    log.info('collect {0}'.format(freq))
+log = logger.getlogger(True)
+
+for fq in freqs:
+    log.info('collect {0}'.format(fq))
 
     df = pd.DataFrame()
     df.index.name = 'Adjacent windows (minutes)' # observation
     df.columns.name = 'Prediction window (minutes)' # prediction
     with Pool(cpu_count() // 2, maxtasksperchild=1) as pool:
-        for i in pool.imap_unordered(accumulate, mkargs(top_level)):
-            log.info('{0} {1}'.format(*i[:2]))
+        for i in pool.imap_unordered(accumulate, mkargs(top_level, fq)):
             df.set_value(*i)
     df = df.ix[df.index.sort_values(),
                df.columns.sort_values(ascending=False)].T
@@ -52,6 +54,6 @@ for freq in args.freqs:
 
     sns.heatmap(df, annot=True, fmt='.0f')
 
-    fname = 'frequency-' + freq
-    dest = target.joinpath(fname).with_suffix('.png'))
+    fname = 'frequency-' + fq
+    dest = target.joinpath(fname).with_suffix('.png')
     plt.savefig(str(dest))
