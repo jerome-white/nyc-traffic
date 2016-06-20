3ebf2981149e348ebd1ce3c88c923bbb98384417
Jerome White <jerome@thewex.net>
2016-06-20 10:58:43 +0400

    Support for creation of multiple resampling frequencies and multiple plot types.

--- src/characterise/occurence-plot.py
+++ src/characterise/occurence-plot.py
@@ -1,3 +1,6 @@
+# import matplotlib
+# matplotlib.style.use('ggplot')
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -7,48 +10,73 @@ from lib import db
 from lib import cli
 from lib import logger
 from pathlib import Path
-
 from multiprocessing import Pool
 from multiprocessing import cpu_count
 
-def mkargs(args):
-    root = Path(args.plotdir)
-    for i in root.iterdir():
-        for j in i.iterdir():
-            if j.is_dir():
-                yield (j, '4H') # args.gfilter)
-
-def plot(path, freq):
-    logger.getlogger().info(str(path))
+def mkargs(source, target, frequencies):
+    heading = [ 'Observation', 'Prediction' ]
     
-    data = [ pd.read_pickle(str(x)) for x in path.glob('*.pkl') ]
+    for observation in source.iterdir():
+        if observation.is_dir():
+            for prediction in observation.iterdir():
+                if prediction.is_dir():
+                    subdir = Path(observation.stem, prediction.stem)
+                    
+                    title = zip(heading, map(int, subdir))
+                    title = ' '.join([ ': '.join(map(str, x)) for x in title ])
+                    
+                    dest = target.joinpath(subdir)
+                    dest.mkdir(parents=True, exist_ok=True)
+
+                    yield (prediction, dest, frequencies, title)
+
+def boxplot(**kwargs):
+    sns.boxplot(palette="PRGn", whis=np.inf, **kwargs)
+    sns.stripplot(jitter=True, size=3, color='.3', linewidth=0, **kwargs)
+
+def factorplot(**kwargs):
+    sns.factorplot(kind='bar', **kwargs)
+
+def plot(source, target, frequencies, title):
+    logger.getlogger().info(str(source))
+    
+    data = [ pd.read_pickle(str(x)) for x in source.glob('*.pkl') ]
     df = pd.concat(data, axis=1)
-    df = df.resample(freq).sum()
+
+    columns = [ 'Hour', 'Segment', 'Average number of jams' ]
+    kwargs = { 'x': columns[0], 'y': columns[-1] }
     
-    grouped = df.groupby(lambda x: x.hour).mean()
-    grouped = grouped.stack(level=0).reset_index()
-    grouped.columns = [ 'Hour', 'Segment', 'Jam average' ]
+    for freq in map(lambda x: str(x) + 'H', frequencies):
+        df = df.resample(freq).sum()
+    
+        grouped = df.groupby(lambda x: x.hour).mean()
+        grouped = grouped.stack(level=0).reset_index()
+        grouped.columns = [ 'Hour', 'Segment', 'Average number of jams' ]
 
-    grouped.replace(to_replace={grouped.columns[0]: {
-        0: '00:00-03:59',
-        4: '04:00-07:59',
-        8: '08:00-11:59',
-        12: '12:00-15:59',
-        16: '16:00-19:59',
-        20: '20:00-23:59',
-        }}, inplace=True)
+        # grouped.replace(to_replace={grouped.columns[0]: {
+        #     0: '00:00-03:59',
+        #     4: '04:00-07:59',
+        #     8: '08:00-11:59',
+        #     12: '12:00-15:59',
+        #     16: '16:00-19:59',
+        #     20: '20:00-23:59',
+        # }}, inplace=True)
 
-    args = { 'x': grouped.columns[0],
-             'y': grouped.columns[-1],
-             'data': grouped }
-    sns.boxplot(palette="PRGn", whis=np.inf, **args)
-    sns.stripplot(jitter=True, size=3, color='.3', linewidth=0, **args)
-    sns.despine(offset=10, trim=True)
+        kwargs['data'] = grouped
+        for f in [ boxplot, factorplot ]:
+            f(args)
+            sns.despine(trim=True)
 
-    fname = str(path.joinpath('boxplot').with_suffix('.png'))
-    plt.gcf().savefig(fname)
+            fname = f.__name__ + '-' + freq
+            dest = target.joinpath(fname).with_suffix('.png')
 
-args = cli.CommandLine(cli.optsfile('prediction-plot')).args
+            plt.title(title)
+            plt.gcf().savefig(str(dest))
+            plt.close()
 
-with Pool(cpu_count() // 2) as pool:
-    pool.starmap(plot, mkargs(args))
+args = cli.CommandLine(cli.optsfile('characterisation-plot')).args
+source = Path(args.source)
+target = Path(args.target)
+
+with Pool(cpu_count() // 2, maxtasksperchild=1) as pool:
+    pool.starmap(plot, mkargs(source, target, args.freqs))
