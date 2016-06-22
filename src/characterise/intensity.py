a64d84c03ce8651f1309fbeb90b42bcf1771b026
Jerome White <jerome@thewex.net>
2016-06-22 08:10:28 +0400

    Prediction window is calculated using the distance between observation and target windows (rather than being taken from the Window object itself).

--- src/characterise/intensity.py
+++ src/characterise/intensity.py
@@ -2,55 +2,64 @@ import itertools
 
 import numpy as np
 import pandas as pd
+import scipy.constants as constant
 
 class Intensity:
-    def __init__(self, readings, prediction, classifier):
+    def __init__(self, readings, classifier):
         self.readings = readings
-        self.prediction = prediction
         self.classifier = classifier
 
+    def slide(self, drange):
+        index = self.readings.index
+        stop = len(index[index >= drange.min()]) - len(drange)
+        
+        yield from map(lambda x: drange + x, range(stop + 1))
+        
     def incomplete(self, data):
         return data.isnull().values.any()
         
     def duration(self, left, right):
         # readings should be complete
-        readings = [ self.readings[x] for x in (right, left) ]
-        if [ self.incomplete(x) for x in readings ].any():
+        (l_reads, r_reads) = [ self.readings[x] for x in (right, left) ]
+        if self.incomplete(l_reads) or self.incomplete(r_reads):
             return np.nan
 
-        return self._duration(readings.pop(), right)
-
-    def slide(self, drange):
-        stop = len(drange.union(self.readings.index)) - len(drange)
-        assert(stop > 0)
+        # Calculate the prediction window size by looking at the
+        # difference between left and right. For this to work, the
+        # frequencies must be in minutes.
+        assert(type(left.freq) == pd.tseries.offsets.Minute)
+        assert(type(right.freq) == pd.tseries.offsets.Minute)
+        delta = right.min() - left.max()
+        delta = delta.total_seconds() / constant.minute
         
-        yield from map(lambda x: drange + x, range(stop + 1))
+        return self._duration(l_reads, right, delta)
         
-    def _duration(self, observation, right):
+    def _duration(self, observation, right, delta):
         raise NotImplementedError
 
 class IncreasingWindow(Intensity):
-    def _duration(self, observation, right):
+    def _duration(self, observation, right, delta):
         lmean = observation.mean()
-
-        for i in range(len(right)):
-            index = right[:i + 1]
+        index = pd.date_range(right.min(), periods=1, freq=right.freq)
+        
+        for i in self.slide(index.copy()):
+            index = index.union(i)
             r = self.readings[index]
             if self.incomplete(r):
                 break
             
             rmean = r.mean() # XXX weighted average?
-            if not self.classifier.classify(self.prediction, lmean, rmean):
+            if not self.classifier.classify(delta, lmean, rmean):
                 break
 
-        return i
+        return len(index) - 1
     
 class SlidingWindow(Intensity):
-    def __init__(self, readings, prediction, classifier, inclusive=True):
-        super().__init__(readings, prediction, classifier)
+    def __init__(self, readings, classifier, inclusive=True):
+        super().__init__(readings, classifier)
         self.inclusive = inclusive
         
-    def _duration(self, observation, right):
+    def _duration(self, observation, right, delta):
         lmean = observation.mean()
         
         for i in self.slide(right):
@@ -60,17 +69,17 @@ class SlidingWindow(Intensity):
                 break
             
             rmean = r.mean() # XXX weighted average?
-            if not self.classifier.classify(self.prediction, lmean, rmean):
+            if not self.classifier.classify(delta, lmean, rmean):
                 break
 
         return len(right.union(i[:-1]))
 
 class StandardDeviation(Intensity):
-    def __init__(self, readings, prediction, classifier, deviations=1):
-        super().__init__(readings, prediction, classifier)
+    def __init__(self, readings, classifier, deviations=1):
+        super().__init__(readings, classifier)
         self.deviations = deviations
         
-    def _duration(self, observation, right):
+    def _duration(self, observation, right, delta):
         epsilon = observation.std()
         
         for i in self.slide(right):
