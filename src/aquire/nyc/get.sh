7a0ee6afe92f757eccda1fed34c241c57d699257
Jerome White <jerome@thewex.net>
2016-07-05 14:02:19 +0400

    NYC data storage location specification consistent with store script.

--- src/aquire/nyc/get.sh
+++ src/aquire/nyc/get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 url=http://207.251.86.229/nyc-links-cams/LinkSpeedQuery.txt
-out=/data/jsw7/traffic/nyc/`date +'%Y-%j/%s'`
+out=$HOME/data/traffic/nyc/`date +'%Y-%j/%s'`
 
 mkdir --parents `dirname $out`
 wget --quiet --output-document=$out $url
