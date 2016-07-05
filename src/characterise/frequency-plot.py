7716a81f9de1625265185023eeec8734017697e1
Jerome White <jerome@thewex.net>
2016-07-05 15:58:59 +0400

    Frequency plot and data gathering name (un)confusion.

--- src/characterise/frequency-plot.py
+++ src/characterise/frequency-plot.py
@@ -1,50 +1,59 @@
-import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-from lib import db
-from lib import cli
-from lib import logger
-from pathlib import Path
-from multiprocessing import Pool
-from multiprocessing import cpu_count
-
-def mkargs(top_level, freq):
-    for i in top_level.iterdir():
-        if i.is_dir():
-            for j in i.iterdir():
-                if j.is_dir():
-                    yield (j, freq)
-
-def acc(args):
-    (path, freq) = args
-    (observation, prediction) = [ int(x) for x in path.parts[-2:] ]
-
-    logger.getlogger().info('o: {0} p: {1}'.format(observation, prediction))
+def pltvar(axis, xy, df, labels, stem):
+    (x, y) = xy
+    (xlabel, ylabel) = labels
     
-    data = [ pd.read_pickle(str(x)) for x in path.glob('*.pkl') ]
-    df = pd.concat(data, axis=1)
-    df = df.resample(freq).sum().mean()
+    df = data.std(axis=axis)
+    df.name = 'deviation'
+    df = df.reset_index()
+    
+    kwargs = { 'x': x, 'y': y, 'data': df }
+    sns.boxplot(palette="PRGn", whis=np.inf, **kwargs)
+    sns.stripplot(jitter=True, size=3, color='.3', linewidth=0, **kwargs)
+    
+    ax = plt.gca()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    
+    dest = source.joinpath('variance-' + stem).with_suffix('.png')
+    plt.savefig(str(dest))
+    plt.close()
 
-    return (observation, prediction, df)
 
 args = cli.CommandLine(cli.optsfile('characterisation-plot')).args
-top_level = Path(args.source)
-target = Path(args.target)
-target.mkdir(parents=True, exist_ok=True)
+source = Path(args.source)
+xlabel = 'Adjacent windows (minutes)'
 
-freqs = args.freqs if args.freqs else [ 'D' ] # XXX defaults?
+for i in map(str, source.glob('*.pkl')):
+    data = pd.from_pickle(i)
+    
+    log.info('visualize: mean')
 
-names = [ 'observation', 'prediction' ]
-log = logger.getlogger(True)
+    df = data.mean(axis=1).unstack('observation')
+    df = df.ix[df.index.sort_values(ascending=False)]
+    sns.heatmap(df, annot=True, fmt='.0f')
+    ax = plt.gca()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel('Prediction window (minutes)')
+    dest = source.joinpath('frequency-' + i.stem).with_suffix('.png')
+    plt.savefig(str(dest))
+    plt.close()
 
-for fq in freqs:
-    log.info('collect {0}'.format(fq))
+    log.info('visualize: variance')    
 
-    with Pool(cpu_count() // 2, maxtasksperchild=1) as pool:
-        f = pool.imap_unordered
-        d = { tuple(i): j.values for (*i, j) in f(acc, mkargs(top_level, fq)) }
-    index = pd.MultiIndex.from_tuples(d.keys(), names=names)
-    df = pd.DataFrame(list(d.values()), index=index).sort_index()
+    kwargs = {
+        'axis': 0,
+        'xy': [ 'deviation', 'observation' ],
+        'labels': [ xlabel, 'Observation window std. dev. (jams/day)' ],
+        'stem': i.stem,
+        }
+    pltvar(**kwargs)
+    
+    kwargs['axis'] += 1
+    kwargs['xy'].reverse()
+    kwargs['labels'][1] = 'Prediction window std. dev. (jams/day)'
 
-    fname = target.joinpath(fq).with_suffix('.pkl')
-    df.to_pickle(str(fname))
+    pltvar(**kwargs)
