9b392824ebc906d0197968db3fe15a279120fbae
Jerome White <jerome@thewex.net>
2016-07-16 17:56:50 +0400

    Smarter retry options for wget

--- src/aquire/nyc/get.sh
+++ src/aquire/nyc/get.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-url=http://207.251.86.229/nyc-links-cams/LinkSpeedQuery.txt
 out=$HOME/data/traffic/nyc/`date +'%Y-%j/%s'`
-
 mkdir --parents `dirname $out`
-wget --quiet --output-document=$out $url
+
+wget \
+    --random-wait \
+    --quiet \
+    --wait=3 \
+    --tries=3 \
+    --timeout=13 \
+    --output-document=$out \
+    http://207.251.86.229/nyc-links-cams/LinkSpeedQuery.txt
