2ec9e2831b0af71c20426b0bd804c8c970535ec6
Jerome White <jerome@thewex.net>
2016-08-22 08:52:49 +0400

    Focused file to functions and classes that are necessary. Continued development/bug fixes.

--- src/prediction/prediction.py
+++ src/prediction/prediction.py
@@ -32,29 +32,6 @@ class Segment:
         self.speed = self.df.speed
         self.speed.name = sid
 
-    def apply(self, df, window, classifier):
-        '''
-        determine whether a window constitutes a traffic event
-        '''
-        assert(type(df) == np.ndarray)
-
-        left_right = []        
-        segments = (df[:window.observation], df[-window.target:])
-        
-        for i in segments:
-            if np.isnan(np.sum(i)):
-                return np.nan
-            left_right.append(i.mean())
-
-        return classifier.classify(window.prediction + 1, *left_right)
-
-    def jams(self, window, classifier):
-        roller = self.df.readings.speed.rolling(len(window))
-        df = roller.apply(self.apply, args=[ window, classifier ])
-        # lag = window.prediction + window.target
-
-        return df.shift(-len(window))
-
 class Cluster(list):
     def group(self, df, window):
         for i in win.idx_range(df.index, size=window.observation):
@@ -69,52 +46,50 @@ class Cluster(list):
         return df
 
 def observe(args):
-    (segment_id, config) = args
+    (data, config) = args
     
     log = logger.getlogger()
-    log(args.segment)
+    log(data.stem)
+
+    root = pd.from_csv(data)
+    rate = root.speed.index.to_series().diff().mean().total_seconds()
+    if rate > float(config['parameters']['intra-reporting']):
+        log.error('{0}: {1}', root.name, rate)
+        return
 
     #
     # Obtain the network...
     #
     depth = int(config['neighbors']['depth'])
     network = Network(config['data']['network'])
-    raw_cluster = list(itertools.islice(network.bfs(args.segment), depth))
+    raw_cluster = list(itertools.islice(network.bfs(root.name), depth))
     if len(raw_cluster) != depth + 1:
-        log.error('{0}: {1} {2}'.format(segment_id, len(segments), depth))
+        log.error('{0}: {1} {2}'.format(root.name, len(raw_cluster), depth))
         return
 
     #
     # ... convert it to segments
     #
     path = Path(config['data']['raw'])
-    segments = []
-    columns = []
-    for (i, j) in enumerate(raw_cluster):
-        for k in j:
-            p = path.joinpath(k).with_suffix('.pkl')
-            df = pd.from_pickle(str(p))
-            if i == 0:
-                rate = df.index.to_series().diff().mean().total_seconds()
-                if rate > float(config['parameters']['intra-reporting']):
-                    log.error('{0}: {1}', segment_id, rate)
-                    return
-            
+    segments = [ root ]
+    for (i, level) in enumerate(raw_cluster):
+        for j in level:
+            p = path.joinpath(j).with_suffix('.csv')
+            df = pd.from_csv(str(p))
+
             lag = df.travel.mean() * i
             df = df.shift(round(lag))
             df.fillna(method='bfill', inplace=True)
         
             segments.append(df.speed)
     cluster = pd.concat(segments)
-    cluster.columns = segment_list
-
-    root = cluster[segment_id]
-    missing = root[root.isnull()].index
-    cluster.interpolate(inplace=True)
     
     #
     #
     #
+    missing = root[root.isnull()].index
+    cluster.interpolate(inplace=True)
+
     observations = []
     window = win.window_from_config(config)
     for i in window.slide(cluster.index):
@@ -128,16 +103,22 @@ def observe(args):
     #
     #
     #
-    with open(config['data']['observations']) as fp:
+    output = Path(config['data']['observations'], segment_id)
+    output = output.with_suffix('.csv')
+    with open(str(output)) as fp:
         writer = csv.writer(fp)
         writer.writerows(observations)
 
 def predict(args):
-    pass
+    (data, config) = args
 
-def enum(config):
-    path = Path(config['data']['raw'])
-    yield from map(lambda x: (int(x.stem), config), path.iterdir('*.pkl')
+    observations = np.loadtxt(data, delimiter=',')
+
+def enum(config, key):
+    path = Path(config['data'][key])
+    yield from map(lambda x: (x, config), path.iterdir('*.csv'))
+
+############################################################################
 
 arguments = ArgumentParser()
 arguments.add_argument('--configuration')
@@ -145,7 +126,12 @@ args = arguments.parse_args()
 config = ConfigParser()
 config.read(args.configuration)
 
+l = OrderedDict([
+        ('raw', observe),
+        ('observations', predict),
+])
+
 with Pool() as pool:
-    for f in [ observe, predict ]:
-        for _ in pool.imap_unordered(f, enum(config)):
+    for (key, f) in l.items():
+        for _ in pool.imap_unordered(f, enum(config, key)):
             pass
