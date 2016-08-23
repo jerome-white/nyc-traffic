16e058096eb9d9d909bf61267fdaa8d4e065a5da
Jerome White <jerome@thewex.net>
2016-08-23 17:08:39 +0400

    Changes made to work with new machine.

--- src/prediction/classifier.py
+++ src/prediction/classifier.py
@@ -1,4 +1,3 @@
-import machine
 import sklearn.metrics
 
 import lib.cpoint as cp
@@ -6,6 +5,7 @@ import lib.aggregator as ag
 import scipy.constants as constant
 
 from lib import logger
+from machine import Machine
 from collections import namedtuple
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
@@ -16,11 +16,11 @@ from sklearn.naive_bayes import GaussianNB
 
 ClassifierFactory = namedtuple('ClassifierFactory', [ 'construct', 'kwargs' ])
 
-class Classifier(machine.Machine):
-    def __init__(self, nid, config, aggregator=ag.simple, jam=cp.Acceleration):
-        super().__init__(nid, config, aggregator)
+class Classifier(Machine):
+    def __init__(self, observations):
+        super().__init__(observations)
         
-        self._metrics = [
+        self.metrics = [
             self.confusion_matrix,
             self.roc,
             sklearn.metrics.accuracy_score,
@@ -28,7 +28,7 @@ class Classifier(machine.Machine):
             sklearn.metrics.matthews_corrcoef,
         ]
 
-        self._machines = {
+        self.machines = {
             'svm': ClassifierFactory(SVC, {
                 'cache_size': 2000, # 2GB
                 # 'probability': True,
@@ -39,68 +39,31 @@ class Classifier(machine.Machine):
             'boost': ClassifierFactory(GradientBoostingClassifier, {}),
             'forest': ClassifierFactory(RandomForestClassifier, {}),
         }
-
-        threshold = float(self.config['parameters']['acceleration'])
-        self.jam_classifier = jam(threshold)
-
+        
     def set_probabilities(self, clf, x):
         try:
-            self.probs = machine.ClsProbs(True, clf.predict_proba(x))
+            self.probabilities = clf.predict_proba(x)
         except AttributeError as err:
             # this generally happens if a classifier doesn't natively
             # support probability estimates (such as SVMs; set
-            # 'probability' in this case)
+            # 'probability' to True in this case)
             self.log.warning(err)
-
-    def roc(self, y_true, y_pred):
-        if self.probs.valid:
-            p = self.probs.probabilities
-            try:
-                (fpr, tpr, _) = sklearn.metrics.roc_curve(y_true, p[:,1])
-                return sklearn.metrics.auc(fpr, tpr)
-            except IndexError:
-                err = 'Invalid probability matrix: {0}'
-                err = err.format(p.shape)
-                raise ValueError(err)
+        
+    def roc(self, y_true, y_pred=None):
+        if self.probabilities is None:
+            return
+        
+        p = self.probabilities
+        try:
+            (fpr, tpr, _) = sklearn.metrics.roc_curve(y_true, p[:,1])
+            return sklearn.metrics.auc(fpr, tpr)
+        except IndexError:
+            msg = 'Invalid probability matrix: {0}'
+            raise ValueError(msg.format(p.shape))
                                                         
     def confusion_matrix(self, y_true, y_pred):
         cm = sklearn.metrics.confusion_matrix(y_true, y_pred)
         cv = cm.flatten()
-        if len(cv) != 4:
-            err = 'Invalid confusion matrix: {0}'.format(cm.shape)
-            raise ValueError(err)
+        assert(len(cv) == self.no_labels ** 2)
 
-        return ','.join(map(str, cv))
-
-    def _features(self, nodes, left):
-        features = []
-        
-        for i in nodes:
-            values = i.readings.speed.ix[left]
-            assert(len(values) == int(self.config['window']['observation']))
-            distilled = self.aggregator(values)
-            features.extend(distilled)
-
-        return features
-
-    def _labels(self, node, left, right):
-        means = []
-        for i in (left, right):
-            series = node.readings.speed.ix[i]
-            nans = series.isnull().values.sum()
-            if nans > 0:
-                msg = '{0}: Incomplete interval: {1}-{2} {3} of {4}'
-                err = msg.format(node, i.min(), i.max(), nans, len(i))
-                raise ValueError(err)
-            means.append(series.mean())
-        (l, r) = means
-
-        gap = right.min() - left.max()
-        duration = gap.total_seconds() / constant.minute
-        
-        label = self.jam_classifier.classify(duration, l, r)
-        
-        return [ int(label) ]
-
-    def legal_stratification(self, strat):
-        return strat.classes.size == self.jam_classifier.categories
+        return ' '.join(map(str, cv))
--- src/prediction/estimator.py
+++ src/prediction/estimator.py
@@ -9,16 +9,16 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.naive_bayes import GaussianNB
 
 class Estimator(Machine):
-    def __init__(self, nid, config, aggregator=ag.simple):
-        super().__init__(nid, config, aggregator)
+    def __init__(self, observations):
+        super().__init__(observations)
         
-        self._metrics = [
+        self.metrics = [
             sklearn.metrics.explained_variance_score,
             sklearn.metrics.mean_absolute_error,
             sklearn.metrics.mean_squared_error,
         ]
 
-        self._machines = {
+        self.machines = {
             'svm': SVR,
             'tree': DecisionTreeRegressor,
             'bayes': GaussianNB,
