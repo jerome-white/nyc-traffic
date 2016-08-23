6adfd82060243a3ea09a58a8e6d5904412d034db
Jerome White <jerome@thewex.net>
2016-08-23 17:12:02 +0400

    Addition of new Network interface (RoadNetwork). Old network left in place for iteration reference.

--- lib/network.py
+++ lib/network.py
@@ -1,3 +1,5 @@
+import csv
+
 from lib import db
 from lib import logger
 from lib import node as nd
@@ -74,3 +76,26 @@ class Network:
     def nodes(self):
         for i in self:
             yield(i.node)
+
+class RoadNetwork(dict):
+    def __init__(self, network_file):
+        with open(network_file) as fp:
+            for row in csv.reader(fp):
+                (key, *row) = list(map(int, row))
+                self[key] = row
+    
+    def _bfs(self, roots, trail):
+        outgoing = []
+        for i in filter(lambda x: x in self, roots):
+            for j in filter(lambda x: x not in trail, self[i]):
+                outgoing.append(j)
+        
+        if outgoing:
+            yield outgoing
+            yield from self._bfs(outgoing, trail.union(outgoing))
+
+    def bfs(self, root, inclusive=False):
+        roots = [ root ]
+        if inclusive:
+            yield roots
+        yield from self._bfs(roots, set())
