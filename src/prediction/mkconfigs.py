9989458b6e4869d3ac3b904e4e1bc6a66e9738ca
Jerome White <jerome@thewex.net>
2016-08-23 17:14:10 +0400

    Moving away from older command line parse interface (back to Python basics).
    Updates for new test-train split options.
    
    Removed per-node configuration. It was intentionally in place to allow
    for distributed operation, but that's a long way off.

--- src/prediction/mkconfigs.py
+++ src/prediction/mkconfigs.py
@@ -8,6 +8,8 @@ import configparser
 from lib import db
 from lib import cli
 from lib import node
+from lib import window
+from argparse import ArgumentParser
 from tempfile import NamedTemporaryFile
 
 # http://stackoverflow.com/a/5228294
@@ -15,12 +17,17 @@ def product(d):
     for i in itertools.product(*d.values()):
         yield dict(zip(d, i))
 
-cargs = cli.CommandLine(cli.optsfile('config')) # /etc/opts/config
-args = cargs.args
+arguments = ArgumentParser()
+arguments.add_argument('--reporting-threshold')
+arguments.add_argument('--output-directory')
+# arguments.add_argument('--parallel')
+arguments.add_argument('--verbose')
+args = arguments.parse_args()
+
 tmpargs = {
     'mode': 'w',
     'delete': False,
-    'dir': args.output,
+    'dir': args.output_directory,
     'prefix': '', # empty string defaults to 'tmp'
     'suffix': '.ini',
 }
@@ -47,59 +54,58 @@ options = {
         # 'average',
         # 'difference',
     ],
+    'change': [
+        # 'percentage',
+        # 'derivative',
+        'acceleration',
+    ],
     'selection': [
         'simple',
         # 'var',
         # 'hybrid',
     ],
-    'observation': [ str(x) for x in [ 12 ] ],
+    'test-train': [],
+    'observation': [ str(x) for x in [ 5 ] ],
     'prediction': [ str(x) for x in [ 3 ] ],
-    'target': [ str(x) for x in [ 8 ] ],
+    'target': [ str(x) for x in [ 5 ] ],
     'depth': [ str(x) for x in range(8) ],
 }
+
+testing_sizes = [ 0.2 ]
+for i in map(lambda x: (x, 1 - x), testing_sizes):
+    j = ','.join(map(str, i))
+    options['test-train'].append(j)
 
 #
 # Build the file!
 #
 p = 'parameters'
 helper = {
-    'window': [ 'observation', 'prediction', 'target' ],
+    'window': window.names_,
     'neighbors': [ 'depth', 'selection' ],
 }
 for (i, o) in enumerate(product(options)):
-    if args.parallel:
-        nodes = [ None ]
-    else:
-        db.genop(args.reporting)
-        nodes = node.getnodes()
-    
-    for n in nodes:
-        config = configparser.ConfigParser()
-        if args.skeleton:
-            config.read(args.skeleton)
+    config = configparser.ConfigParser()
+    (test, train) =  o['test-train'].split(',')
 
-        for (title, keys) in helper.items():
-            config[title] = { k: o[k] for k in keys }
+    for (title, keys) in helper.items():
+        config[title] = { k: o[k] for k in keys }
         
-        config['machine'] = {
-            'folds': str(10),
-            'model': 'classification',
-            'method': ','.join(machines),
-            'feature-transform': o['transform'],
-        }
+    config['machine'] = {
+        'folds': str(10),
+        'model': 'classification',
+        'method': ','.join(machines),
+        'feature-transform': o['transform'],
+        'change-point': o['change'],
+        'testing': test,
+    }
 
-        config['output'] = {
-            'print-header': str(i == 0),
-        }
+    config[p] = {
+        'acceleration': str(-0.002),
+        'intra-reporting': args.reporting_threshold,
+    }
 
-        config[p] = {
-            'acceleration': str(-0.002),
-            'intra-reporting': args.reporting,
-        }
-        if n is not None:
-            config[p]['node'] = str(n)
-
-        with NamedTemporaryFile(**tmpargs) as fp:
-            if args.verbose:
-                print(fp.name)
-            config.write(fp)
+    with NamedTemporaryFile(**tmpargs) as fp:
+        if args.verbose:
+            print(fp.name)
+        config.write(fp)
