25317832a043f8e90ef5ca1f5be5bd2e3c4b9ec1
Jerome White <jerome@thewex.net>
2016-08-23 17:13:33 +0400

    Updates to work with new prediction interface and file semantics.

--- src/prediction/main.sh
+++ src/prediction/main.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-mkcurrent() {
-    cd $1
-    rm --force current
-    ln --symbolic `basename $out` current
-}
-
-phome=$NYCTRAFFIC/src/prediction
+p_home=$NYCTRAFFIC/src/prediction
+p_log=$NYCTRAFFICLOG/prediction/test
+while getopts "r:n:" OPTION; do
+    case $OPTION in
+        r) readings=$OPTARG ;;
+        n) network=$OPTARG ;;
+        *) exit 1 ;;
+    esac
+done
+mkdir --parents $p_log
 
 #
 # Create the output directory
 #
 while true; do
-    out=$NYCTRAFFICLOG/prediction/`date +%Y_%m-%d_%H%M`.`hostname`
-    if [ ! -e $out ]; then
-        mkdir --parents $out/ini
-        ( mkcurrent `dirname $out` )
+    base=$p_log/`date +%Y_%m-%d_%H%M`.`hostname`
+    if [ ! -e $base ]; then
+        mkdir --parents $base
         break
     fi
     # sleep until this minute is finished
@@ -25,20 +27,31 @@ done
 #
 # Begin the run!
 #
-python3 $phome/mkconfigs.py \
+tmp=`mktemp --directory --tmpdir=$base ini.XXX`
+python3 $p_home/mkconfigs.py \
     --reporting-threshold 120 \
-    --output-directory $out/ini \
-    --skeleton $NYCTRAFFIC/etc/opts/prediction.ini \
-    --parallel \
-    --quiet
+    --output-directory $tmp
 
 i=0
-configs=( `ls $out/ini/*` )
+configs=( `ls $tmp/*` )
 for j in ${configs[@]}; do
-    echo "[ `date` : $i / ${#configs[@]} ] $j " >> $out/trace
+    echo "[ `date` : $i / ${#configs[@]} ] $j " >> $base/trace
+
+    d=$base/`basename $j .ini`
+    mkdir --parents $d
+    for k in observations results; do
+        mkdir $d/$k
+    done
+    (cat $j; cat <<EOF) > $d/ini
+[data]
+raw = $readings
+network = $network
+observations = $d/observations
+results = $d/results
+EOF
     
-    grep --quiet 'node' $j || python3 $NYCTRAFFIC/etc/db/mkopt.py --config $j
-    python3 $phome/main.py --config $j
+    python3 $p_home/prediction.py --config $d/ini
     
     (( i++ ))
-done > $out/dat 2> $out/log
+done
+rm --recursive --force $tmp
