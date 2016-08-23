c08977c3fad6731032ca7c8de9b7529f8360a0c2
Jerome White <jerome@thewex.net>
2016-08-23 17:09:00 +0400

    Machine is now a shell of its former self. It is only responsible for operations dealing with machine learning, not building the observation set.
    Classifier and machine now rolled into the machine.

--- src/prediction/machine.py
+++ src/prediction/machine.py
@@ -1,214 +1,152 @@
-import pickle
 import warnings
+import sklearn.metrics
 
 import numpy as np
-import lib.cluster as cl
-import lib.network as nt
+
+import lib.cpoint as cp
+import lib.aggregator as ag
+import scipy.constants as constant
 
 from lib import logger
-from lib import configtools
 from tempfile import NamedTemporaryFile
-from lib.window import Window
 from collections import namedtuple
 from sklearn.metrics.base import UndefinedMetricWarning
 from sklearn.cross_validation import StratifiedShuffleSplit
 
-ClsProbs = namedtuple('ClsProbs', [ 'valid', 'probabilities' ])
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.dummy import DummyClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.naive_bayes import GaussianNB
+
+Data = namedtuple('Data', 'x_train, x_test, y_train, y_test')
+ClassifierFactory = namedtuple('ClassifierFactory', [ 'construct', 'kwargs' ])
+
+Selector = lambda x: {
+    'classification': Classifier,
+    'estimation': Estimator,
+}[x]
 
 class Machine:
-    def __init__(self, nid, config, aggregator):
-        self.nid = nid
-        self.config = config
-        self.aggregator = aggregator
-        self.network = None
-        self.log = logger.getlogger()
+    def __init__(self, observations):
+        assert(np.isfinite(observations).all())
 
-        self.nhandler = {
-            'simple': cl.Cluster,
-            'var': cl.VARCluster,
-            'hybrid': cl.HybridCluster,
-            }
-        
-        self._metrics = []
-        self._header = [
-            'implementation',
-            'shape',
-            'node',
-            'network',
-            'kfold',
-            ]
-        self._machines = {}
-        self.probs = ClsProbs(False, None)
-        
-    def classify(self):
-        w = [ 'observation', 'prediction', 'target' ]
-        window = Window(*[ int(self.config['window'][x]) for x in w ])
-        
-        #
-        # Create the network of nodes (the source and its neighbors)
-        #
-        self.log.info('{0} + create network'.format(self.nid))
-        n = self.config['neighbors']
-        cluster = self.nhandler[n['selection']]
-        self.network = nt.Network(self.nid, int(n['depth']), cluster)
-        self.log.info('{0} - create network'.format(self.nid))
-        
-        depth = self.network.depth()
-        if depth != int(n['depth']):
-            msg = 'Depth not fully explored: {0} < {1}'
-            raise ValueError(msg.format(depth, n['depth']))
-        
-        root = self.network.node
-        assert(root.nid == self.nid)
-        
-        #
-        # Clean up the nodes: All nodes are shifted, interpolated, and
-        # time-aligned with the root. Missing values at the root are
-        # maintained to determine which windows are valid to process.
-        #
-        s = root.readings.speed
-        missing = s[s.isnull()].index
-        
-        for i in self.network:
-            n = i.node
-            n.readings.shift(i.lag)
-            n.align(root, True)
-                
-        #
-        # Build the observation matrix by looping over the root nodes
-        # time periods
-        #
-        self.log.info('{0} + observe'.format(self.nid))
-        observations = []
-        for i in root.range(window):
-            whole = [ missing.intersection(x).size == 0 for x in i ]
-            if all(whole):
-                (left, right) = i
-                mid = left[-len(right):]
-                assert(len(mid) == len(right))
-                
-                labels = self._labels(root, mid, right)
-                features = self._features(self.network.nodes(), left)
-                
-                observations.append(features + labels)
-        self.log.info('{0} - observe {1}'.format(self.nid, len(observations)))
-                
-        return observations
+        self.features = observations[:,:-1]
+        self.labels = observations[:,-1:].ravel()
+        self.no_labels = len(np.unique(self.labels))
 
-    #
-    # Splits the data into training and test sets. Returns stratified
-    # portions as a generator
-    #
-    def stratify(self, observations, folds, testing=0.2, no_labels=1):
-        assert(0 < testing < 1)
-
-        data = np.asfarray(observations)
-        assert(np.isfinite(data).all())
-
-        labels = data[:,-no_labels:].ravel()
-        s = StratifiedShuffleSplit(labels, n_iter=folds, test_size=testing)
-        if not self.legal_stratification(s):
-            raise StopIteration
-        features = data[:,:-no_labels]
+        self.metrics = []
+        self.machines = {}
+        self.probabilities = None
         
-        for (train, test) in s:
-            x = (features[train], features[test], labels[train], labels[test])
-            yield x
+    def stratify(self, folds, test_size=0.2):
+        assert(0 < test_size < 1)
 
-    #
-    # Cycle through requested learning methods. Returns an instance
-    # of the method and its name as a generator
-    #
+        stratifications = StratifiedShuffleSplit(self.labels, n_iter=folds,
+                                                 test_size=test_size)
+        for (train, test) in stratifications:
+            yield Data(self.features[train], self.features[test],
+                       self.labels[train], self.labels[test])
+
     def machinate(self, methods):
         wanted = set(methods.split(','))
-        for i in wanted.intersection(self._machines.keys()):
-            factory = self._machines[i]
+        for i in wanted.intersection(self.machines.keys()):
+            factory = self.machines[i]
             instance = factory.construct(**factory.kwargs)
             
             yield (factory.construct.__name__, instance)
-            
-    def predict(self, observations):
-        predictions = []
-        network = repr(self.network)
-        args = self.config['machine']
 
-        #
-        # build k stratifications
-        #
-        stratifications = self.stratify(observations, int(args['folds']))
-
-        #
-        # for each machine, train/predict using each stratification
-        #
-        self.log.info('{0} + prediction'.format(self.nid))
-        for (i, j) in enumerate(stratifications):
-            (x_train, x_test, y_train, y_test) = j
-
-            for (name, clf) in self.machinate(args['method']):
-                #
-                # train and fit the model
-                #
-                try:
-                    clf.fit(x_train, y_train)
-                    pred = clf.predict(x_test)
-                except (AttributeError, ValueError) as error:
-                    with NamedTemporaryFile(mode='wb', delete=False) as fp:
-                        pickle.dump(observations, fp)
-                        msg = [ name, error, fp.name ]
-                        self.log.error('{0}: {1} {2}'.format(*msg))
-                    continue
-                
-                self.set_probabilities(clf, x_test)
-                
-                #
-                # add accounting information to result row
-                #
-                lst = [
-                    name,          # implementation
-                    x_train.shape, # shape
-                    self.nid,      # node
-                    network,       # network
-                    i,             # (k)fold
-                    ]
-                assert(len(lst) == len(self._header))
-                
-                d = dict(zip(self._header, lst))
-                d.update(configtools.ordered(self.config))
-                
-                #
-                # run all of the desired metrics and add them to the
-                # result row
-                #
-                with warnings.catch_warnings():
-                    warnings.filterwarnings('error')
-                    for f in self._metrics:
-                        try:
-                            d[f.__name__] = f(y_test, pred)
-                        except (UndefinedMetricWarning, ValueError) as error:
-                            self.log.warning(error)
-                    
-                predictions.append(d)
-        self.log.info('{0} prediction {1}'.format(self.nid, len(predictions)))
+    def train(self, clf, data):
+        clf.fit(data.x_train, data.y_train)
         
-        return predictions
+        return clf.predict(data.x_test)
 
-    def header(self):
-        h = configtools.ordered(self.config)
-        return self._header + list(h.keys()) + self.metrics()
-
-    def metrics(self):
-        return [ x.__name__ for x in self._metrics ]
-
-    # Abstract methods
-
-    def _features(self, nodes, left):
-        raise NotImplementedError()
-    
-    def _labels(self, node, left, right):
-        raise NotImplementedError()
+    def predict(self, data, pred):
+        with warnings.catch_warnings():
+            warnings.filterwarnings('error')
+            for f in self.metrics:
+                try:
+                    result = f(data.y_test, pred)
+                except (UndefinedMetricWarning, ValueError) as error:
+                    log.warning(error)
+                    result = None
+                    
+                yield (f.__name__, result)
 
     def set_probabilities(self, clf, x):
         raise NotImplementedError()
 
-    def legal_stratification(self, strat):
-        raise NotImplementedError()
+class Classifier(Machine):
+    def __init__(self, observations):
+        super().__init__(observations)
+        
+        self.metrics = [
+            self.confusion_matrix,
+            self.roc,
+            sklearn.metrics.accuracy_score,
+            sklearn.metrics.f1_score,
+            sklearn.metrics.matthews_corrcoef,
+        ]
+
+        self.machines = {
+            'svm': ClassifierFactory(SVC, {
+                'cache_size': 2000, # 2GB
+                # 'probability': True,
+            }),
+            'tree': ClassifierFactory(DecisionTreeClassifier, {}),
+            'bayes': ClassifierFactory(GaussianNB, {}),
+            'dummy': ClassifierFactory(DummyClassifier, {}),
+            'boost': ClassifierFactory(GradientBoostingClassifier, {}),
+            'forest': ClassifierFactory(RandomForestClassifier, {}),
+        }
+        
+    def set_probabilities(self, clf, x):
+        try:
+            self.probabilities = clf.predict_proba(x)
+        except AttributeError as err:
+            # this generally happens if a classifier doesn't natively
+            # support probability estimates (such as SVMs; set
+            # 'probability' to True in this case)
+            self.log.warning(err)
+        
+    def roc(self, y_true, y_pred=None):
+        if self.probabilities is None:
+            return
+        
+        p = self.probabilities
+        try:
+            (fpr, tpr, _) = sklearn.metrics.roc_curve(y_true, p[:,1])
+            return sklearn.metrics.auc(fpr, tpr)
+        except IndexError:
+            msg = 'Invalid probability matrix: {0}'
+            raise ValueError(msg.format(p.shape))
+                                                        
+    def confusion_matrix(self, y_true, y_pred):
+        cm = sklearn.metrics.confusion_matrix(y_true, y_pred)
+        cv = cm.flatten()
+        assert(len(cv) == self.no_labels ** 2)
+
+        return ' '.join(map(str, cv))
+
+class Estimator(Machine):
+    def __init__(self, observations):
+        super().__init__(observations)
+        
+        self.metrics = [
+            sklearn.metrics.explained_variance_score,
+            sklearn.metrics.mean_absolute_error,
+            sklearn.metrics.mean_squared_error,
+        ]
+
+        self.machines = {
+            'svm': SVR,
+            'tree': DecisionTreeRegressor,
+            'bayes': GaussianNB,
+            'forest': RandomForestRegressor,
+        }
