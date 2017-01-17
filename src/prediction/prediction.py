10c9acd1b0da0775dcb39269f3dcad0328712a6e
Jerome White <jerome@thewex.net>
2017-01-17 23:13:02 +0400

    Segment and cluster moved to library.

--- src/prediction/prediction.py
+++ src/prediction/prediction.py
@@ -34,47 +34,6 @@ class Writer:
         fname = path.joinpath(str(segment_id)).with_suffix('.' + suffix)
 
         self.fp = fname.open('w')
-        
-class Segment:
-    def __init__(self, csv_file, name=None, freq='T'):
-        columns = [ 'speed', 'travel' ]
-        df = pd.read_csv(str(csv_file),
-                         index_col='as_of',
-                         parse_dates=True,
-                         dtype={ x: np.float64 for x in columns })
-        
-        self.frequency = df.index.to_series().diff().mean().total_seconds()
-        self.df = df.resample(freq).mean()
-        self.name = name if name is not None else csv_file.stem
-
-    def __str__(self):
-        return self.name
-
-    # def lag(self, with_respect_to, multiplier):
-    #     lag = self.df.travel.mean() * multiplier
-    #     self.df = self.df.shift(round(lag))
-    #     self.df.fillna(method='bfill', inplace=True)
-
-class Cluster(list):
-    def __init__(self, root):
-        super().__init__()
-        self.append(root)
-        
-    def combine(self, interpolate=True):
-        reference = self[0].df.index
-        
-        df = pd.concat([ x.df.speed for x in self ], axis=1)
-        df = df.resample(reference.freq).mean()
-        df = df.loc[reference.min():reference.max()]
-        df.columns = [ x.name for x in self ]
-        
-        if interpolate:
-            df.interpolate(inplace=True)
-            for i in 'bf':
-                method = i + 'fill'
-                df.fillna(method=method, inplace=True)
-            
-        return df
 
 def observe(args):
     log = logger.getlogger()
