958a1c41e10d71e3f806e180d7a53f34bcb5bd9f
Jerome White <jerome@thewex.net>
2017-02-10 01:57:52 -0500

    Frequency script now known as classify.

--- etc/qsub/classify.sh
+++ etc/qsub/classify.sh
@@ -36,7 +36,7 @@ for i in `seq $nodes`; do
     tmp=`mktemp`
     ( >&2 echo "[ `date` ] $i $tmp" )
 cat <<EOF > $tmp
-python $HOME/src/nyc-traffic/src/characterise/frequency.py $frequency $alpha \
+python $HOME/src/nyc-traffic/src/characterise/classify.py $frequency $alpha \
   --data $SCRATCH/nyc/data \
   --output $output \
   --ledger $ledger \
