45936a566a5e447a931fe488bc3c82c370a54c90
Jerome White <jerome@thewex.net>
2017-02-15 11:43:44 +0400

    Data can be saved and re-read. Ability to set heatmap min/max values of heatmap.

--- src/visualization/plots/heatmap.py
+++ src/visualization/plots/heatmap.py
@@ -1,8 +1,8 @@
+import operator as op
 from pathlib import Path
 from argparse import ArgumentParser
 from multiprocessing import Pool
 
-import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -32,6 +32,10 @@ def func(args):
 arguments = ArgumentParser()
 arguments.add_argument('--data', type=Path)
 arguments.add_argument('--output', type=Path)
+arguments.add_argument('--save-data', type=Path)
+arguments.add_argument('--from-data', type=Path)
+arguments.add_argument('--vmax', type=float)
+arguments.add_argument('--vmin', type=float)
 # http://pandas.pydata.org/pandas-docs/stable/timeseries.html#offset-aliases
 arguments.add_argument('--frequency', default='D')
 args = arguments.parse_args()
@@ -39,13 +43,21 @@ args = arguments.parse_args()
 log = logger.getlogger(True)
 
 columns = [ 'observation', 'offset', 'mean' ]
-with Pool() as pool:
-    iterable = map(lambda x: (x, args.frequency), args.data.glob('**/*.csv'))
-    data = filter(None, pool.imap_unordered(func, iterable))
-    df = pd.DataFrame.from_records(data, columns=columns)
+if args.from_data:
+    df = pd.read_csv(str(args.from_data), index_col=0)
+else:
+    with Pool() as pool:
+        glob = args.data.glob('**/*.csv')
+        iterable = map(lambda x: (x, args.frequency), glob)
+        data = filter(None, pool.imap_unordered(func, iterable))
+        df = pd.DataFrame.from_records(data, columns=columns)
+    if args.save_data:
+        df.to_csv(str(args.save_data))
+
 groups = df.groupby(by=columns[:-1])
 table = groups.mean().unstack()
 table.columns = table.columns.droplevel() # http://stackoverflow.com/a/22233719
 
-ax = sns.heatmap(table, annot=True, fmt='.0f')
+kwargs = { x: op.attrgetter(x)(args) for x in ('vmin', 'vmax') }
+ax = sns.heatmap(table, annot=True, fmt='.0f', **kwargs)
 ax.figure.savefig(str(args.output))
