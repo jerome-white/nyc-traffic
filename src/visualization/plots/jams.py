8894b086db33b7844ca4a8df99fffd2410498a84
Jerome White <jerome@thewex.net>
2018-05-25 19:52:09 +0400

    Plot figures for each gravitational threshold.

--- src/visualization/plots/jams.py
+++ src/visualization/plots/jams.py
@@ -1,39 +1,42 @@
-import itertools as it
 import multiprocessing as mp
-from pathlib import Path
 from argparse import ArgumentParser
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from lib import logger
 from lib.window import Window
 from lib.cpoint import Acceleration
 
 def func(args):
-    (gravity, offset, limit) = args
+    (gravity, offsets, limit) = args
 
     log = logger.getlogger()
     acc = Acceleration(gravity)
-    win = Window(offset=offset)
+    cutoffs = np.full((offsets, limit), np.nan)
 
-    cutoffs = [ np.nan ] * limit
-    observations = [ np.nan ] * len(win)
+    for o in range(offsets):
+        log.debug('{0} {1}'.format(gravity, o))
 
-    for start in range(limit):
-        observations[0] = start
-        for stop in range(limit):
-            observations[-1] = stop
-            if not acc.classify(observations, win):
-                cutoffs[start] = stop
-                break
+        win = Window(offset=o+1)
+        observations = [ np.nan ] * len(win)
 
-    return (gravity, offset, cutoffs)
+        for start in range(limit):
+            observations[0] = start
+            for stop in range(limit):
+                observations[-1] = stop
+                if not acc.classify(observations, win):
+                    cutoffs[o, start] = stop
+                    break
+
+    df = pd.DataFrame(cutoffs.T, columns=range(1, offsets + 1))
+
+    return (gravity, df)
 
 def each(args):
     for i in range(4, 4 * 10, 4):
-        gravity = i / 1e4
-        for j in range(args.offsets):
-            yield (gravity, j + 1, args.speed_limit)
+        yield (i / 1e4, args.offsets, args.speed_limit)
 
 arguments = ArgumentParser()
 arguments.add_argument('--offsets', type=int)
@@ -44,5 +47,9 @@ args = arguments.parse_args()
 log = logger.getlogger(True)
 
 with mp.Pool(args.workers) as pool:
-    for i in pool.imap_unordered(func, each(args)):
-        print(i)
+    for (i, j) in pool.imap_unordered(func, each(args)):
+        log.info(i)
+
+        title = str(i)
+        j.plot.line(grid=True, title=title)
+        plt.savefig(title + '.png')
